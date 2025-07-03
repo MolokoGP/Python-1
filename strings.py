e8cf7c387677314a8a623f1672c883f80fa54b68
MolokoGP <31440639+MolokoGP@users.noreply.github.com>
2025-07-03 13:09:11 +0200

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,8 @@
+#Advanced concepts - Strings
+
+message = ' Hello, world! '
+
+print(message.strip()) # Remove leading and trailing whitespaces
+print(message.lower()) # convert all characters to lowercase
+print(message.split(',')) # spit the string into a list based on the comma
+print(message.upper()) #converts all characters to uppercase
---
+++ variables.py
@@ -0,0 +1,6 @@
+#variables
+
+my_variable = 10
+total_count = 0
+user = 'John'
+
